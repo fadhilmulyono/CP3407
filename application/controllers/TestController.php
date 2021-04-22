4091db13106a1c22aab9d86b5d1e5b43bf158a9a
Fadhil Mulyono <fadhilmulyono@gmail.com>
2021-04-22 21:41:35 +0700

    Application framework

---
+++ application/controllers/TestController.php
@@ -0,0 +1,10 @@
+<?php
+    class TestController extends CI_Controller {
+        public function __construct() {
+            parent::__construct();
+        }
+
+        public function index() {
+            $this->load->view("testview");
+        }
+    }
